e743ed4c9f98f480eca808aee0f5a4fb855e175c
k.vamshi <kvamshi13032003@gmail.com>
2026-02-26 12:24:18 +0530

    Fixed Reg.jsx state issue - production build working

--- src/Reg.jsx
+++ src/Reg.jsx
@@ -1,38 +1,42 @@
 import axios from "axios";
 import { useState } from "react";
-function Reg()
-{
-    const [name,setName]=useState(
-        {
-            username:"",
-            email:"",
-            password:""     
-        }
-    )
-    const changeName=(e)=>
-    {
-        setdata({...name,[e.target.name]:e.target.value})
-    }
-    const submit=async()=>
-    {
-        try{
-            const res=await axios.post("http://cabsysystem-1.onrender.com/register",data)
-            alert(res.data)
-        }
-        catch(xyz)
-        {
-            alert(xyz.response?.data||"Error")
-        }
-    }
-    return(
-        <>
-        <h1>Hey iam App </h1>
-        <input onChange={changeName} name="username" placeholder="Enter username"/>
-        <input onChange={changeName} name="email" placeholder="Enter email"/>
-        <input onChange={changeName} name="password" placeholder="Enter password"/>
-        <button onClick={submit}>Register</button> 
-        </>     
-    )
 
+function Reg() {
+
+    const [name, setName] = useState({
+        username: "",
+        email: "",
+        password: ""
+    });
+
+    const changeName = (e) => {
+        setName({
+            ...name,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const submit = async () => {
+        try {
+            const res = await axios.post(
+                "http://cabsysystem-1.onrender.com/register",
+                name   // ✅ use name instead of data
+            );
+            alert(res.data);
+        } catch (xyz) {
+            alert(xyz.response?.data || "Error");
+        }
+    };
+
+    return (
+        <>
+            <h1>Hey I am App</h1>
+            <input onChange={changeName} name="username" placeholder="Enter username" />
+            <input onChange={changeName} name="email" placeholder="Enter email" />
+            <input onChange={changeName} name="password" placeholder="Enter password" />
+            <button onClick={submit}>Register</button>
+        </>
+    );
 }
+
 export default Reg;

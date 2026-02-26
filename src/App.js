e3d39f06c90a171b0ba867240200cb7de2114b2c
k.vamshi <kvamshi13032003@gmail.com>
2026-02-26 11:54:37 +0530

    Frontend initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Home from "./Home"
+import Reg from "./reg"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+    <>
+    <h1>Iam App</h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/h" element={<Home/>}/>
+      <Route path="/reg" element={<Reg/>}/>
+    </Routes>
+    </BrowserRouter>  
+    </>
+  )
+}
 export default App;
---
+++ src/Home.jsx
@@ -0,0 +1,9 @@
+function Home()
+{
+    return(
+        <>
+        <h1>Hey Iam Home....</h1>
+        </> 
+    )
+}
+export default Home;
